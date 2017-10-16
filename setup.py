db8803566a06c90902a790e9a32cc760e47d8bbc
Gopikrishna <S.Gopikrishna@thomsonreuters.com>
2017-10-16 15:39:32 -0400

    First commit'

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='sg-utils',
+    version='0.1.0',
+    author='Shashank Gopikrishna',
+    author_email='shashank.gopikrishna@gmail.com',
+    packages=['sg-utils'],
+    description='Useful utility classes and functions',
+)

6df6a05709d8d54b9ba18673e5f202d5a0cf0895
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-10-23 00:43:07 -0400

    0.1.0

--- sg_utils/utils.py
+++ sg_utils/utils.py
@@ -1,5 +1,14 @@
+import string, random
+
 class dotdict(dict):
 	"""dot.notation access to dictionary attributes"""
 	__getattr__ = dict.get
 	__setattr__ = dict.__setitem__
 	__delattr__ = dict.__delitem__
+
+
+
+def giberish(size=64):
+    """Generates salt for password encryption"""
+    alph = str(string.lowercase+string.digits)*2
+    return str(''.join(random.sample(alph, size)))

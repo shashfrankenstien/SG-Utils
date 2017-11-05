23952f60baafd2cc7001213ef1da096378bb1333
Shashank Gopikrisna <shashank.gopikrishna@gmail.com>
2017-11-05 16:21:42 -0500

    python safe dict iter

--- sg_utils/utils.py
+++ sg_utils/utils.py
@@ -1,4 +1,4 @@
-import string, random
+import string, random, sys
 
 class dotdict(dict):
 	"""dot.notation access to dictionary attributes"""
@@ -7,8 +7,14 @@ class dotdict(dict):
 	__delattr__ = dict.__delitem__
 
 
+def iterdict(d):
+	if sys.version_info.major==3:
+		return d.items()
+	else:
+		return d.iteritems()
+
 
 def giberish(size=64):
-    """Generates salt for password encryption"""
-    alph = str(string.lowercase+string.digits)*2
-    return str(''.join(random.sample(alph, size)))
+	"""Generates salt for password encryption"""
+	alph = str(string.lowercase+string.digits)*2
+	return str(''.join(random.sample(alph, size)))

2726f7eec4dcd8e4989e0be8e3d60d621f51bfe7
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2018-06-25 14:55:46 -0400

    Update setup.py - Auto install dependencies

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     author='Shashank Gopikrishna',
     author_email='shashank.gopikrishna@gmail.com',
     packages=['sg_utils'],
+    install_requires=['flask', 'holidays'],
     description='Useful utility classes and functions',
 )
